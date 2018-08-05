987b46ef3c24c1da2743288db6ebad918fdf2ec2
surobinda <surobinda@gmail.com>
2018-08-05 22:08:59 +0530

    second test commit

--- src/main/java/com/boot/repository/ShipwreckRepository.java
+++ src/main/java/com/boot/repository/ShipwreckRepository.java
@@ -2,6 +2,7 @@ package com.boot.repository;
 
 import com.boot.model.Shipwreck;
 import org.springframework.data.jpa.repository.JpaRepository;
+/* test 2 commit*/
 
 public interface ShipwreckRepository extends JpaRepository<Shipwreck, Long> {
 }
